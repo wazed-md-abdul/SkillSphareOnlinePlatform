87b0d8d61bc8442024b1c5dce72c68bfca0106f1
wazed-md-abdul <wazedjiad@gmail.com>
2026-05-02 23:19:44 +0800

    bugs fixed for deployment

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 export const getAllCourses = async () => {
-    const res = await fetch("https://skill-sphare-online-platform.vercel.app/data.json");
+    const res = await fetch("https://skill-sphare-onlinelearningplatform.vercel.app/data.json");
     const data = await res.json();
     return data;
 
